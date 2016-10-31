4f7668918b2dfc7ff8dd7d63a3cde859473a4a6a
martindestefanis <tinchodestefanis1@gmail.com>
2016-10-31 17:02:05 -0300

    probando

--- Trabajo Práctico (Capas)/CPresentación/Formularios/f02-RealizarCuestionario.cs
+++ Trabajo Práctico (Capas)/CPresentación/Formularios/f02-RealizarCuestionario.cs
@@ -58,5 +58,10 @@ namespace Trabajo_práctico
             f21_CuestionarioInstrucciones cui = new f21_CuestionarioInstrucciones();
             cui.Show(this);
         }
+
+        private void f2_RealizarCuestionario_Load(object sender, EventArgs e)
+        {
+            //probando
+        }
     }
 }

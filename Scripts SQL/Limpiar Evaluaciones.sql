0238220921b03afaa42a365529de9c1ad80abbd3
fedealbertengo <falbertengo@frsf.utn.edu.ar>
2016-12-12 14:54:48 -0300

    Agregada Carpeta para los Sripts SQL

---
+++ Scripts SQL/Limpiar Evaluaciones.sql
@@ -0,0 +1,13 @@
+﻿DELETE FROM Evaluacion_Candidato;
+
+DELETE FROM Estado_Cuestionario;
+
+DELETE FROM ItemBloque;
+
+DELETE FROM Bloque;
+
+DELETE FROM Cuestionario;
+
+DELETE FROM LogProcesos;
+
+SELECT * FROM Candidato;
